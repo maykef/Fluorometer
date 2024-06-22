77316f015bdcce9059f30cbc97ac7435d10c3932
Mayke De Freitas <maykef@gmail.com>
2024-06-22 15:07:29 +0100

    Update SI7021_datalogger_relay.ino

--- Arduino/SI7021_datalogger_relay/SI7021_datalogger_relay.ino
+++ Arduino/SI7021_datalogger_relay/SI7021_datalogger_relay.ino
@@ -5,21 +5,27 @@
 
 Adafruit_Si7021 sensor = Adafruit_Si7021();
 
-
 const int chipSelect = 10;  // used for Arduino
-int relay = 11;
-
+const int relayPin = 8;
 RTC_PCF8523 rtc;
 
+unsigned long previousMillis = 0;
+unsigned long relayMillis = 0;
+const long interval = 1000;  // 1 second
+const long relayOnTime = 10000;  // 10 seconds
+
+bool relayState = false;
 
 void setup() {
+  pinMode(relayPin, OUTPUT);
+  digitalWrite(relayPin, LOW);
   Serial.begin(115200);
   
   while (!Serial) {
     delay(100);
   }
 
-   if (!rtc.begin()) {
+  if (!rtc.begin()) {
     Serial.println("Couldn't find RTC");
     while (1);
   }
@@ -30,7 +36,7 @@ void setup() {
   }
 
   if (!rtc.isrunning()) {
-    Serial.println("RTC lost power, lets set the time!");
+    Serial.println("RTC lost power, let's set the time!");
     rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
   }
 
@@ -38,58 +44,46 @@ void setup() {
     Serial.println("Initialization failed!");
     while (1);
   }
-
-  Serial.print("Found model ");
-  switch(sensor.getModel()) {
-    case SI_Engineering_Samples:
-      Serial.print("SI engineering samples"); break;
-    case SI_7013:
-      Serial.print("Si7013"); break;
-    case SI_7020:
-      Serial.print("Si7020"); break;
-    case SI_7021:
-      Serial.print("Si7021"); break;
-    case SI_UNKNOWN:
-    default:
-      Serial.print("Unknown");
-  }
-  Serial.print(" Rev(");
-  Serial.print(sensor.getRevision());
-  Serial.print(")");
-  Serial.print(" Serial #"); Serial.print(sensor.sernum_a, HEX); Serial.println(sensor.sernum_b, HEX);
 }
 
 void loop() {
-  
-  String dataString = "";
+  unsigned long currentMillis = millis();
 
-  float h = sensor.readHumidity();
-  float t = sensor.readTemperature();
+  // Log data every second
+  if (currentMillis - previousMillis >= interval) {
+    previousMillis = currentMillis;
 
-  DateTime now = rtc.now();
+    float h = sensor.readHumidity();
+    float t = sensor.readTemperature();
 
-  dataString += String(now.unixtime());
-  dataString += ",";
-  dataString += String(h);
-  dataString += ",";
-  dataString += String(t);
-  
-  File dataFile = SD.open("datalog.txt", FILE_WRITE);
+    DateTime now = rtc.now();
 
-  if (dataFile) {
-    dataFile.println(dataString);
-    dataFile.close();
-    Serial.println(dataString);
-    if (h > 75) {
-      digitalWrite (relay, HIGH);
+    String dataString = "";
+    dataString += String(h);
+    dataString += ",";
+    dataString += String(t);
+    
+    File dataFile = SD.open("datalog.txt", FILE_WRITE);
+
+    if (dataFile) {
+      dataFile.println(dataString);
+      dataFile.close();
+      Serial.println(dataString);
+    } else {
+      Serial.println("error opening datalog.txt");
     }
-    else if (h < 65) {
-      digitalWrite (relay, LOW);
+
+    // Control the relay based on humidity
+    if (h > 82 && !relayState) {
+      digitalWrite(relayPin, HIGH);
+      relayState = true;
+      relayMillis = currentMillis;
     }
   }
-  else {
-    Serial.println("error opening datalog.txt");
-  }
 
-  delay(20000);
+  // Turn off the relay after 10 seconds
+  if (relayState && currentMillis - relayMillis >= relayOnTime) {
+    digitalWrite(relayPin, LOW);
+    relayState = false;
+  }
 }
